9e7539ec8c0ef2f69b2ff845008b75aed03c83dd
ParasP41 <parasvishwakarma198@gmail.com>
2025-01-03 23:39:08 +0530

    6-hours

---
+++ car.js
@@ -0,0 +1,23 @@
+// const ford={
+//     brancd:"Ford",
+//     model:"Fiesta"
+// }
+
+// const tesla={
+//     branch:"new york",
+//     model:"Model S"
+// }
+
+exports.ford={
+    brancd:"Ford",
+    model:"Fiesta"
+}
+
+exports.tesla={
+    branch:"new york",
+    model:"Model S"
+}
+
+// module.exports=ford;
+// exports.data={ford,tesla};
+
--- index.js
+++ index.js
@@ -3,3 +3,27 @@ console.log("Paras");
 
 // process.exit(0); //to exit the code
 // process.exitCode=1; //to exit the code
+
+
+
+// import lodash from "lodash";
+// let arr=[1,2,3,4,5];
+// // console.log(lodash.chunk(arr));
+// console.log(lodash.last(arr));
+
+// import cowsay from "cowsay"
+// console.log(cowsay.say({
+//     text: "Hello, World!",
+//     e: "oO",
+//     T: "U "  // U for upside-down cow
+// }));
+
+// import car from "./car";
+// const car=require("./car");
+// console.log(car);
+// const { data } =require("./car");
+// console.log(data) ;
+
+const {tesla,ford} =require ("./car");
+console.log(JSON.stringify(tesla,null,2)) ;
+console.log(ford)
