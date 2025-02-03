e610ae20cae14df0756db399be118191d977972d
ParasP41 <parasvishwakarma198@gmail.com>
2025-02-03 18:27:21 +0530

    6-hours

--- Ch-10/exp-gen/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express tutorial',message:"welcome to my PC" });
-});
-
-module.exports = router;
